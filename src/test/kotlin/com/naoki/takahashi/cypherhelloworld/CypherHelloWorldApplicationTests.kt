82d346534a91b228ee030e80a867e197381dce79
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-11-09 14:54:42 +0900

    first commit

---
+++ src/test/kotlin/com/naoki/takahashi/cypherhelloworld/CypherHelloWorldApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.naoki.takahashi.cypherhelloworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CypherHelloWorldApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
