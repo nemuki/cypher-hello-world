4bba817d3cab82592656edf9e2f0d5ccf6ceb175
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-11-10 11:09:41 +0900

    `/` にHello World APIを作成 #2

--- src/main/kotlin/com/naoki/takahashi/cypherhelloworld/CypherHelloWorldApplication.kt
+++ src/main/kotlin/com/naoki/takahashi/cypherhelloworld/CypherHelloWorldApplication.kt
@@ -2,9 +2,20 @@ package com.naoki.takahashi.cypherhelloworld
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class CypherHelloWorldApplication
+@RestController
+class CypherHelloWorldApplication {
+    @GetMapping("/")
+    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String): HelloWorld {
+        return HelloWorld(
+            message = "Hello, $name"
+        )
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<CypherHelloWorldApplication>(*args)
