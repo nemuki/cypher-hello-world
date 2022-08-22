1f25fe8bdf2b86f4020be82e942866ea1e5c2537
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-08-22 10:57:07 +0900

    init project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cypher-hello-world
+
 Cypher: Spring BootCamp Hello World API
+
+## 開発環境
+
+```
+MacBook Pro (13-inch, M1, 2020)
+
+IDE: IntelliJ IDEA
+JDK: Amazon corretto-11.0.16 x86_64
+```
---
+++ src/test/kotlin/com/nemuki/cypherhelloworld/CypherHelloWorldApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nemuki.cypherhelloworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CypherHelloWorldApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
