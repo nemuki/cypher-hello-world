220970db3d74dedcecfdcac404ef03250133a42e
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-09-05 14:16:15 +0900

    packageを分けた

--- src/main/kotlin/com/nemuki/cypherhelloworld/controller/ErrorController.kt
+++ src/main/kotlin/com/nemuki/cypherhelloworld/controller/ErrorController.kt
@@ -1,5 +1,6 @@
-package com.nemuki.cypherhelloworld
+package com.nemuki.cypherhelloworld.controller
 
+import com.nemuki.cypherhelloworld.response.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
