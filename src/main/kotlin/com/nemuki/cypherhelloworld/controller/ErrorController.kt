56f6983237dfe74c024b97296e976afe488d0188
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-09-05 15:37:46 +0900

    Applicationの例外を拾う

--- src/main/kotlin/com/nemuki/cypherhelloworld/controller/ErrorController.kt
+++ src/main/kotlin/com/nemuki/cypherhelloworld/controller/ErrorController.kt
@@ -2,6 +2,7 @@ package com.nemuki.cypherhelloworld.controller
 
 import com.nemuki.cypherhelloworld.response.ErrorResponse
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -12,4 +13,7 @@ class ErrorController {
     @ExceptionHandler(NoHandlerFoundException::class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     fun errorRequest() = ErrorResponse("no handler found")
+
+    @ExceptionHandler(Exception::class)
+    fun applicationException() = ResponseEntity(ErrorResponse("something wrong ;-("), HttpStatus.INTERNAL_SERVER_ERROR)
 }
