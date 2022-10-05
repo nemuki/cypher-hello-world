ebef58983a1596e466aa6f661e764c0754efc38f
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-05 18:47:30 +0900

    リクエストにValidationを追加

--- src/main/kotlin/com/nemuki/cypherhelloworld/controller/HelloWorld.kt
+++ src/main/kotlin/com/nemuki/cypherhelloworld/controller/HelloWorld.kt
@@ -1,12 +1,15 @@
 package com.nemuki.cypherhelloworld.controller
 
 import com.nemuki.cypherhelloworld.response.Message
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.constraints.Size
 
+@Validated
 @RestController
 class HelloWorld {
     @GetMapping("/")
-    fun hello(@RequestParam(value = "name") name: String) = Message("Hello, $name")
+    fun hello(@RequestParam(value = "name") @Size(min = 3, max = 10) name: String) = Message("Hello, $name")
 }
