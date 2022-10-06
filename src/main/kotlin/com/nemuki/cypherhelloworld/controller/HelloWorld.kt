cabf1e532ed009219bcef50b24a2a472e59815d2
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-06 17:44:44 +0900

    HelloWorld で text の読み取り

--- src/main/kotlin/com/nemuki/cypherhelloworld/controller/HelloWorld.kt
+++ src/main/kotlin/com/nemuki/cypherhelloworld/controller/HelloWorld.kt
@@ -1,6 +1,8 @@
 package com.nemuki.cypherhelloworld.controller
 
 import com.nemuki.cypherhelloworld.response.Message
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController
 import javax.validation.constraints.Size
 
 @Validated
+@Configuration
 @RestController
-class HelloWorld {
+class HelloWorld(
+    @Value("\${text}") private val text: String
+) {
     @GetMapping("/")
-    fun hello(@RequestParam(value = "name") @Size(min = 3, max = 10) name: String) = Message("Hello, $name")
+    fun hello(@RequestParam(value = "name") @Size(min = 3, max = 10) name: String) = Message("Hello $text, $name")
 }
